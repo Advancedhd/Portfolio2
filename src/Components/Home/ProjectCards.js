e66302a340b798312c77fb041a2eb4d80288aeab
Advancedhd <hakon.volden@gmail.com>
2022-08-09 14:16:23 +0200

    Update ProjectCards.js

--- src/Components/Home/ProjectCards.js
+++ src/Components/Home/ProjectCards.js
@@ -17,8 +17,10 @@ function ProjectCards() {
               <Card.Body>
                 <Card.Title>Javascript Frameworks</Card.Title>
                 <Card.Text>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
+                  This project was made for the sole purpose of practicing
+                  creating a axios login form with the npm package yup for
+                  validation and react hook form. And using a fetch get request
+                  to get games from the api url.
                 </Card.Text>
                 <Button
                   variant="primary"
@@ -44,8 +46,10 @@ function ProjectCards() {
               <Card.Body>
                 <Card.Title>NordicShoeStore</Card.Title>
                 <Card.Text>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
+                  The site is made using HTML for viewing, JS for functionality,
+                  sass for styling. The clients can view and shop for shoes. The
+                  admin of the page can POST/DELETE/PUT shoes into the Strapi
+                  headless CMS it uses.
                 </Card.Text>
                 <Button
                   variant="primary"
@@ -63,15 +67,25 @@ function ProjectCards() {
               <Card.Body>
                 <Card.Title>Holidaze</Card.Title>
                 <Card.Text>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
+                  The project Holidaze is a website that tries to deliver
+                  housing to people around a certain area. It has Login
+                  functionality. Uses Strapi as headless CMS. ADD/POST/DELETE if
+                  you are admin and logged in.
                 </Card.Text>
                 <Button
                   variant="primary"
-                  href="https://github.com/Noroff-FEU-Assignments/project-exam-2-Advancedhd"
+                  href="https://github.com/Advancedhd/Project-Holidaze"
                   target="_blank"
                 >
                   Github
+                </Button>
+                <Button
+                  className="site__button"
+                  variant="primary"
+                  href="https://preeminent-gingersnap-cf7d0b.netlify.app/"
+                  target="_blank"
+                >
+                  Site
                 </Button>
               </Card.Body>
             </Card>
